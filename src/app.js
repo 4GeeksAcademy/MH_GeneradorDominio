95fac9f2db39aa2b63657e8ed4ef34c2453f21e8
mhavliczek <32149001+mhavliczek@users.noreply.github.com>
2024-08-30 00:20:47 +0000

    cambios en codigo

--- src/app.js
+++ src/app.js
@@ -6,21 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let pronoun = ['el', 'La','Los','ellos','su'];
-  let adj = ['Gran', 'Grande','Rojo','Chicos','Gordos'];
-  let noun = ['Comilones', 'Bestias','Starks','Salchichas','Peloscrespos'];
-  let dominios =['com','es','dev','info','io'];
-  
-   
-    for(index1 in pronoun){
-        for(index2 in adj){
-            for(index3 in noun){
-                for(index4 in dominios){
-                    console.log(`${pronoun[index1]}${adj[index2]}${noun[index3]}.${dominios[index3]}`);
-                }
-                
-            }
-          }
+  let pronoun = ["el", "La", "Los", "ellos", "su"];
+  let adj = ["Gran", "Grande", "Rojo", "Chicos", "Gordos"];
+  let noun = ["Comilones", "Bestias", "Starks", "Salchichas", "Peloscrespos"];
+  let dominios = ["com", "es", "dev", "info", "io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let m = 0; m < dominios.length; m++) {
+          console.log(`${pronoun[i]}${adj[j]}${noun[k]}.${dominios[m]}`);
         }
-    };
-   
+      }
+    }
+  }
+};
